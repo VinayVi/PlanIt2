7529c53c4d9ea3493473e44852140f48be532863
Connor <cxbivens17@gmail.com>
2017-04-08 14:49:41 -0400

    added 2 activities, the event list, and some drawable shapes

---
+++ app/src/main/java/com/pterabit/planit/RecentlyViewedActivity.java
@@ -0,0 +1,13 @@
+package com.pterabit.planit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecentlyViewedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recently_viewed);
+    }
+}
